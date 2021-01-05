c98a5ab416204987a01b0a05674a14bc2f4a1c32
Pranav Arora <48170643+pranavarora1895@users.noreply.github.com>
2021-01-05 11:56:28 +0530

    Version 1.1

--- find_key.py
+++ find_key.py
@@ -1,9 +1,18 @@
+"""
+EXCEL AND EMAIL AUTOMATION
+
+Step 1: Goto myaccount.google.com/lesssecureapps and toggle the Less Secure Apps to allow python to send the
+emails for you.
+Step 2: Setup your email address and password as your environment variables to maintain your privacy.
+
+"""
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from openpyxl import load_workbook
 import os
 import smtplib
 
+# setting email and password as env variables for privacy purposes
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
@@ -37,13 +46,18 @@ wb = load_workbook(path)
 ws = wb.active
 unique_id = []
 
+# getting the index number of email.
+email_idx = None
+for row in ws.iter_rows(min_row=1, max_row=1, values_only=True):
+    email_idx = row.index('email')
+
 # crawling through all rows, and if the keyword matches then it will send the email. One email per email-id
 # irrespective of number of occurrences
 for row in ws.iter_rows(min_row=2, values_only=True):
     if 'Approved' in row:
-        if row[0] in unique_id:
+        if row[email_idx] in unique_id:
             continue
         else:
-            mail_sending_to_approved(row[0])
-            unique_id.append(row[0])
+            mail_sending_to_approved(row[email_idx])
+            unique_id.append(row[email_idx])
 print(f'Email sent to {unique_id}')
