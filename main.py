5fe5e68051a91f75892c58edda8cd886e0fd593f
Pranav Arora <48170643+pranavarora1895@users.noreply.github.com>
2021-01-05 14:38:50 +0530

    Version 1.3

--- main.py
+++ main.py
@@ -8,56 +8,79 @@ Step 2: Setup your email address and password as your environment variables to m
 """
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+from tkinter import messagebox
 from openpyxl import load_workbook
 import os
 import smtplib
 
 # setting email and password as env variables for privacy purposes
+from openpyxl.utils.exceptions import InvalidFileException
+
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+try:
+    def get_excel_path():
+        """Getting the file (GUI)"""
+        Tk().withdraw()
+        return askopenfilename(filetypes=[('Excel Files', '*.csv;*.xlsx;*.xlsm;*.xltx;*.xltm')],
+                               title='Select your Excel File and Click Open')
+    def get_email_body_path():
+        """Getting the file (GUI)"""
+        Tk().withdraw()
+        return askopenfilename(filetypes=[('All Files','*.*')],
+                               title='Select your E-mail Body File and Click Open')
+
+    path = get_excel_path()
+    email_body = get_email_body_path()
+    print('Processing......')
+    def mail_sending_to_approved(receiver_email_id):
+        """Function to send emails"""
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+            subject = 'Congrats!! You got approved!'
+            body = open(email_body, 'r').read()
+
+            msg = f'Subject: {subject}\n\n{body}'
+
+            smtp.sendmail(EMAIL_ADDRESS, receiver_email_id, msg)
 
 
-def mail_sending_to_approved(receiver_email_id):
-    """Function to send emails"""
 
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    wb = load_workbook(path)
+    ws = wb.active
+    unique_id = []
 
-        subject = 'Congrats!! You got approved!'
-        body = open('mailbody.txt', 'r').read()
+    # getting the index number of email.
+    email_idx = None
+    for row in ws.iter_rows(min_row=1, max_row=1, values_only=True):
+        email_idx = row.index('email')
 
-        msg = f'Subject: {subject}\n\n{body}'
-
-        smtp.sendmail(EMAIL_ADDRESS, receiver_email_id, msg)
-
-
-def get_excel_path():
-    """Getting the file (GUI)"""
-    Tk().withdraw()
-    return askopenfilename(filetypes=[('Excel Files', '*.csv;*.xlsx;*.xlsm;*.xltx;*.xltm')],
-                           title='Select your Excel File and Click Open')
-
-
-path = get_excel_path()
-wb = load_workbook(path)
-ws = wb.active
-unique_id = []
-
-# getting the index number of email.
-email_idx = None
-for row in ws.iter_rows(min_row=1, max_row=1, values_only=True):
-    email_idx = row.index('email')
-
-# crawling through all rows, and if the keyword matches then it will send the email. One email per email-id
-# irrespective of number of occurrences
-for row in ws.iter_rows(min_row=2, values_only=True):
-    if 'Approved' in row:
-        if row[email_idx] in unique_id:
-            continue
-        else:
-            mail_sending_to_approved(row[email_idx])
-            unique_id.append(row[email_idx])
-print(f'Email sent to {unique_id}')
+    # crawling through all rows, and if the keyword matches then it will send the email. One email per email-id
+    # irrespective of number of occurrences
+    keyword = input('Enter your keyword: ')
+    print('Matching the keywords.......')
+    print('Sending them mail......')
+    for row in ws.iter_rows(min_row=2, values_only=True):
+        if keyword in row:
+            if row[email_idx] in unique_id:
+                continue
+            else:
+                mail_sending_to_approved(row[email_idx])
+                unique_id.append(row[email_idx])
+    if not unique_id:
+        messagebox.showinfo("Email not Sent!!", "Your keyword didn't match with any id.")
+    else:
+        messagebox.showinfo("Email Sent!!", f'Email sent to {unique_id}')
+except FileNotFoundError as e:
+    print(f'Error: {e.errno}\nFile Not Found')
+except OSError as o:
+    print(f'Error: {o.errno}\nInvalid Entry')
+except ValueError as v:
+    print('ValueError')
+except InvalidFileException as e:
+    print('Cancelled')
